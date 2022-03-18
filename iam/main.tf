56ac630c02247a5925a9391ec8e09f86108dc39f
tejap <btejapramod@gmail.com>
2022-03-18 12:44:13 +0530

    New Changes

--- iam/main.tf
+++ iam/main.tf
@@ -1,112 +1,92 @@
-resource "aws_iam_user" "stadmin" {
-  count = length(var.storage_user)
-  name  = element(var.storage_user,count.index)
-}
-
-resource "aws_iam_user" "compadmin" {
-  count = length(var.compute_user)
-  name  = element(var.compute_user,count.index)
-}
-resource "aws_iam_group" "sta-groups" {
-  count = length(var.storage_group)
-  name  = element(var.storage_group,count.index)
-}
-resource "aws_iam_group" "comp-groups" {
-  count = length(var.compute_group)
-  name  = element(var.compute_group,count.index)
-}
-resource "aws_iam_user_group_membership" "storage_admin1" {
-  user = element(aws_iam_user.stadmin.name)
-  groups = [
-    aws_iam_group.sta-groups.name
-  ]
-}
-resource "aws_iam_user_group_membership" "compu_admin1" {
-  user = element(aws_iam_user.compadmin.name)
-  groups = [
-    aws_iam_group.comp-groups.name
-  ]
-}
-
-
-output "user_arn" {
-  value = aws_iam_user.stadmin.*.arn
-}
-
-
-output "stagroup_arn" {
-  value = aws_iam_group.sta-groups.*.arn
-}
-
-output "compgroup_arn" {
-  value = aws_iam_group.comp-groups.*.arn
-}
-
-
+#resource "aws_iam_user" "stadmin" {
+#  count = length(var.storage_user)
+#  name  = element(var.storage_user,count.index)
+#}
+#
+#resource "aws_iam_user" "compadmin" {
+#  count = length(var.compute_user)
+#  name  = element(var.compute_user,count.index)
+#}
+#resource "aws_iam_group" "sta-groups" {
+#  count = length(var.storage_group)
+#  name  = element(var.storage_group,count.index)
+#}
+#resource "aws_iam_group" "comp-groups" {
+#  count = length(var.compute_group)
+#  name  = element(var.compute_group,count.index)
+#}
 #resource "aws_iam_user_group_membership" "storage_admin1" {
-#  user = aws_iam_user.stadmin1.name
+#  user = element(aws_iam_user.stadmin.name)
 #  groups = [
-#    aws_iam_group.storage-admin.name
+#    aws_iam_group.sta-groups.name
 #  ]
 #}
-#resource "aws_iam_user_group_membership" "storage_admin2" {
-#  user = aws_iam_user.stadmin2.name
+#resource "aws_iam_user_group_membership" "compu_admin1" {
+#  user = element(aws_iam_user.compadmin.name)
 #  groups = [
-#    aws_iam_group.storage-admin.name
+#    aws_iam_group.comp-groups.name
 #  ]
 #}
-#resource "aws_iam_user_group_membership" "compute-admin1" {
-#  user = aws_iam_user.compadmin1.name
-#  groups = [
-#    aws_iam_group.compute-admin.name
-#  ]
-#}
-#resource "aws_iam_user_group_membership" "compute-admin2" {
-#  user = aws_iam_user.compadmin2.name
-#  groups = [
-#    aws_iam_group.compute-admin.name
-#  ]
-#}
-#resource "aws_iam_user" "stadmin1" {
-#  name = "stadmin1"
+#
+#
+#output "user_arn" {
+#  value = aws_iam_user.stadmin.*.arn
 #}
 #
-#resource "aws_iam_user" "stadmin2" {
-#  name = "stadmin2"
+#
+#output "stagroup_arn" {
+#  value = aws_iam_group.sta-groups.*.arn
 #}
 #
-#resource "aws_iam_user" "compadmin1" {
-#  name = "compadmin1"
+#output "compgroup_arn" {
+#  value = aws_iam_group.comp-groups.*.arn
 #}
-#
-#resource "aws_iam_user" "compadmin2" {
-#  name = "compadmin2"
-#}
-#
-#resource "aws_iam_group" "storage-admin" {
-#  name = "storage-admin"
-#}
-#
-#resource "aws_iam_group" "compute-admin" {
-#  name = "compute-admin"
-#}
-#
-#resource "aws_iam_user_policy" "stadmin1" {
-#  name = "test"
-#  user = aws_iam_user.lb.name
-#
-#  # Terraform's "jsonencode" function converts a
-#  # Terraform expression result to valid JSON syntax.
-#  policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = [
-#          "ec2:Describe*",
-#        ]
-#        Effect   = "Allow"
-#        Resource = "*"
-#      },
-#    ]
-#  })
-#}
+
+
+resource "aws_iam_user_group_membership" "storage_admin1" {
+  user = aws_iam_user.stadmin1.name
+  groups = [
+    aws_iam_group.storage-admin.name
+  ]
+}
+resource "aws_iam_user_group_membership" "storage_admin2" {
+  user = aws_iam_user.stadmin2.name
+  groups = [
+    aws_iam_group.storage-admin.name
+  ]
+}
+resource "aws_iam_user_group_membership" "compute-admin1" {
+  user = aws_iam_user.compadmin1.name
+  groups = [
+    aws_iam_group.compute-admin.name
+  ]
+}
+resource "aws_iam_user_group_membership" "compute-admin2" {
+  user = aws_iam_user.compadmin2.name
+  groups = [
+    aws_iam_group.compute-admin.name
+  ]
+}
+resource "aws_iam_user" "stadmin1" {
+  name = "stadmin1"
+}
+
+resource "aws_iam_user" "stadmin2" {
+  name = "stadmin2"
+}
+
+resource "aws_iam_user" "compadmin1" {
+  name = "compadmin1"
+}
+
+resource "aws_iam_user" "compadmin2" {
+  name = "compadmin2"
+}
+
+resource "aws_iam_group" "storage-admin" {
+  name = "storage-admin"
+}
+
+resource "aws_iam_group" "compute-admin" {
+  name = "compute-admin"
+}
--- iam/variables.tf
+++ iam/variables.tf
@@ -1,18 +1,18 @@
-variable "storage_user" {
-  type = list(string)
-  default = ["stadmin1","stadmin2","stadmin1"]
-}
-variable "compute_user" {
-  type = list(string)
-  default = ["compadmin1","compadmin1","compadmin1"]
-}
-
-variable "storage_group" {
-  type = list(string)
-  default = ["storage-admin"]
-}
-
-variable "compute_group" {
-  type = list(string)
-  default = ["compute-admin"]
-}
+#variable "storage_user" {
+#  type = list(string)
+#  default = ["stadmin1","stadmin2","stadmin1"]
+#}
+#variable "compute_user" {
+#  type = list(string)
+#  default = ["compadmin1","compadmin1","compadmin1"]
+#}
+#
+#variable "storage_group" {
+#  type = list(string)
+#  default = ["storage-admin"]
+#}
+#
+#variable "compute_group" {
+#  type = list(string)
+#  default = ["compute-admin"]
+#}
