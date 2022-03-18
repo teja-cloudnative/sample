6ac61609463fd4a2a5c6c1e27807ff9fcdf1ec23
tejap <btejapramod@gmail.com>
2022-03-18 11:43:50 +0530

    New Changes

--- iam/main.tf
+++ iam/main.tf
@@ -7,7 +7,11 @@ output "user_arn" {
   value = aws_iam_user.demo.*.arn
 }
 
-resource "aws_iam_group" "storage_admin" {
+resource "aws_iam_group" "demo-groups" {
   count = length(var.group)
   name  = element(var.group,count.index)
+}
+
+output "group_arn" {
+  value = aws_iam_group.demo-groups.*.arn
 }
